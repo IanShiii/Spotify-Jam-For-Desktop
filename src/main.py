c5d42ba827183cc15822c8429e18703d366fcdd5
Ian Shi <Ianshi1026@gmail.com>
2024-02-25 13:26:24 -0500

    stopped using recursion in update function, added a way for users to forcefully switch to a new song thats not in queue.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 import subprocess
 import json
-from dotenv import load_dotenv
-import sys
-from processes import join
 
-load_dotenv()
+from processes import join
 
 with open('src/config/config.json', 'r') as configs:
     cookies = json.load(configs)
@@ -14,5 +11,4 @@ with open('src/config/config.json', 'r') as configs:
 USER1_TOKEN = json.loads(subprocess.run('node src/token_access/getTokens.js', capture_output=True, text=True).stdout)['user1']
 USER2_TOKEN = json.loads(subprocess.run('node src/token_access/getTokens.js', capture_output=True, text=True).stdout)['user2']
 
-sys.setrecursionlimit(2000) #LMAO
 join(USER1_TOKEN, USER2_TOKEN)
--- src/processes.py
+++ src/processes.py
@@ -84,6 +84,7 @@ def resume(token):
     requests.put(url='https://api.spotify.com/v1/me/player/play', headers=headers)
 
 # gives priority to user 1 (if both are playing a track, they join user 1)
+# joins, then starts the sync cycle
 def join(user1_token, user2_token):
     user1_is_active, user1_track = get_current_track_info(user1_token)
     user2_is_active, user2_track = get_current_track_info(user2_token)
@@ -104,7 +105,7 @@ def join(user1_token, user2_token):
             'position_ms': user1_track['progress']
         }
         requests.put(url='https://api.spotify.com/v1/me/player/play', headers=headers, json=json_data)
-        update(user1_token, user2_token, 1)
+        sync(user1_token, user2_token, 1)
     elif user2_track['is_playing']:
         headers = {
             'Authorization': f'Bearer {user1_token}',
@@ -115,25 +116,69 @@ def join(user1_token, user2_token):
             'position_ms': user2_track['progress']
         }
         requests.put(url='https://api.spotify.com/v1/me/player/play', headers=headers, json=json_data)
-        update(user1_token, user2_token, 2)
+        sync(user1_token, user2_token, 2)
     else:
         print('waiting for someone to start a track')
         sleep(2)
         join(user1_token, user2_token)
 
-# if both users add songs at the same time, thats probably bad. maybe fixed by calling more frequent?
-def update(user1_token, user2_token, host_user):
+def join_to_specific_user(host_token, joiner_token):
+    host_is_active, host_track = get_current_track_info(host_token)
+    joiner_is_active, joiner_track = get_current_track_info(joiner_token)
+
+    if not (host_is_active and joiner_is_active):
+        print('waiting for both users to have active devices')
+        sleep(2)
+        join_to_specific_user(host_token, joiner_token)
+        return
+    
+    if host_track['is_playing']:
+        headers = {
+            'Authorization': f'Bearer {joiner_token}',
+            'Content-Type': 'application/json'
+        }
+        json_data = {
+            'uris': [host_track['uri']],
+            'position_ms': host_track['progress']
+        }
+        requests.put(url='https://api.spotify.com/v1/me/player/play', headers=headers, json=json_data)
+    else:
+        print('waiting for host to start a track')
+        sleep(2)
+        join_to_specific_user(host_token, joiner_token)
+
+def sync(user1_token, user2_token, host_user):
     if host_user == 1:
         user1_initial_track = get_current_track_info(user1_token)[1]
-        update_unwrapped(user1_token, user2_token, False, user1_initial_track['uri'], user1_initial_track['progress'], user1_initial_track['uri'])
+        is_playing = False
+        prev_song_uri = user1_initial_track['uri']
+        prev_progress = user1_initial_track['progress']
+        current_track_uri = user1_initial_track['uri']
+        while True:
+            new_values = update(user1_token, user2_token, is_playing, prev_song_uri, prev_progress, current_track_uri)
+            is_playing = new_values[0]
+            prev_song_uri = new_values[1]
+            prev_progress = new_values[2]
+            current_track_uri = new_values[3]
+            sleep(.7)
     elif host_user == 2:
         user2_initial_track = get_current_track_info(user2_token)[1]
-        update_unwrapped(user1_token, user2_token, False, user2_initial_track['uri'], user2_initial_track['progress'], user2_initial_track['uri'])
+        is_playing = False
+        prev_song_uri = user2_initial_track['uri']
+        prev_progress = user2_initial_track['progress']
+        current_track_uri = user2_initial_track['uri']
+        while True:
+            new_values = update(user1_token, user2_token, is_playing, prev_song_uri, prev_progress, current_track_uri)
+            is_playing = new_values[0]
+            prev_song_uri = new_values[1]
+            prev_progress = new_values[2]
+            current_track_uri = new_values[3]
+            sleep(.7)
     else:
         print('invalid host_user')
 
-# how do you make wrapper functions lol
-def update_unwrapped(user1_token, user2_token, is_playing, prev_song_uri, prev_progress, initial_track_uri):
+# return tuple (is_playing, prev_song_uri, prev_progress, current_track_uri)
+def update(user1_token, user2_token, is_playing, prev_song_uri, prev_progress, initial_track_uri):
     print('loop')
     user1_track = get_current_track_info(user1_token)[1]
     user2_track = get_current_track_info(user2_token)[1]
@@ -141,7 +186,7 @@ def update_unwrapped(user1_token, user2_token, is_playing, prev_song_uri, prev_p
     user2_queue = get_queue(user2_token, initial_track_uri)
 
     #update the queue
-    #limited to adding one song per cycle, so cant add songs too fast
+    #only one person can add to queue per cycle, in order to keep both in sync
     if user1_track['uri'] == user2_track['uri']:
         for song in user1_queue:
             if song not in user2_queue:
@@ -151,14 +196,23 @@ def update_unwrapped(user1_token, user2_token, is_playing, prev_song_uri, prev_p
             if song not in user1_queue:
                 add_to_queue(user1_token, song)
 
-    #allows for changing songs by checking if the tracks arent the same
-    buffer_ms = 2500
+    #allows for changing/skipping songs by checking if the tracks arent the same
+    buffer_ms = 3000
+    new_track_uri = initial_track_uri
     if user1_track['uri'] != user2_track['uri']:
         if not user1_track['duration'] - user1_track['progress'] < buffer_ms or user2_track['duration'] - user2_track['progress'] < buffer_ms:
             if user1_track['uri'] != prev_song_uri:
-                skip_to_next(user2_token)
+                if user1_track['uri'] != user2_queue[0]:
+                    join_to_specific_user(user1_token, user2_token)
+                    new_track_uri = user1_track['uri']
+                else:
+                    skip_to_next(user2_token)
             elif user2_track['uri'] != prev_song_uri:
-                skip_to_next(user1_token)
+                if user2_track['uri'] != user1_queue[0]:
+                    join_to_specific_user(user2_token, user1_token)
+                    new_track_uri = user2_track['uri']
+                else:
+                    skip_to_next(user1_token)
                 
     # align the progress for both players, incase they are too far away from each other
     # also allows for users to change the progress of the song
@@ -180,30 +234,20 @@ def update_unwrapped(user1_token, user2_token, is_playing, prev_song_uri, prev_p
         prev_song_uri = user2_track['uri']
 
     #allows for pausing/resuming
-    #also the recursive step
-    sleep_time = .7
     if is_playing:
         if not user1_track['is_playing']:
             pause(user2_token)
-            sleep(.7)
-            update_unwrapped(user1_token, user2_token, False, prev_song_uri, prev_progress, initial_track_uri)
+            is_playing = False
         elif not user2_track['is_playing']:
             pause(user1_token)
-            sleep(.7)
-            update_unwrapped(user1_token, user2_token, False, prev_song_uri, prev_progress, initial_track_uri)
-        else:
-            sleep(.7)
-            update_unwrapped(user1_token, user2_token, is_playing, prev_song_uri, prev_progress, initial_track_uri)
+            is_playing = False
     else:
         if user1_track['is_playing']:
             resume(user2_token)
-            sleep(.7)
-            update_unwrapped(user1_token, user2_token, True, prev_song_uri, prev_progress, initial_track_uri)
+            is_playing = True
         elif user2_track['is_playing']:
             resume(user1_token)
-            sleep(.7)
-            update_unwrapped(user1_token, user2_token, True, prev_song_uri, prev_progress, initial_track_uri)
-        else:
-            sleep(.7)
-            update_unwrapped(user1_token, user2_token, is_playing, prev_song_uri, prev_progress, initial_track_uri)
+            is_playing = True
+    
+    return is_playing, prev_song_uri, prev_progress, new_track_uri
             
